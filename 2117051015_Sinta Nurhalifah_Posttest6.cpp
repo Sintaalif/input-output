f15bd46c5c7e01126279d299c8d24285e3b5d3d5
sinta nurhalifah <92354983+sintaaliffzzy@users.noreply.github.com>
2021-11-17 10:40:16 +0700

    Add files via upload

---
+++ 2117051015_Sinta Nurhalifah_Posttest6.cpp
@@ -0,0 +1,21 @@
+#include <ncurses/ncurses.h>
+#include <windows.h>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+initscr();
+
+mvprintw(1,5,"Sedang Loading..\n");
+printw("\n");
+
+for(int a=1;a<30;a++){
+				printw("|");
+				refresh();
+				Sleep(1000);
+		}
+	puts("Sinta Nurhalifah\n");//saya menggunakan puts karena jika menggunakan printw tidak keluar outputnya
+	
+	getch;
+	endwin();
+}
