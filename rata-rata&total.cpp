2e5c72807fd42db5b4963b41d09e01e835d2771f
sinta nurhalifah <92354983+sinskuyyz@users.noreply.github.com>
2021-11-18 11:44:57 +0700

    Add files via upload

---
+++ rata-rata&total.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int array[0], i,n;
+  float rata, total=0;
+    
+    
+  printf("");
+  scanf("%d",&n);    
+    
+  for(i=0; i<n; i++){
+    printf("",i+1);
+    scanf("%d", &array[i]);
+    total = total + array[i];
+
+  }
+    
+  rata = total/n;
+  printf("total: %f \n", total);
+  printf("rata-rata: %f \n", rata);
+    
+  return 0;
+}
