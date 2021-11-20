0927bc252b2baddb395dfe214c6ef00fdffce752
sinta nurhalifah <92354983+sinskuyyz@users.noreply.github.com>
2021-11-20 10:06:14 +0700

    Add files via upload

---
+++ Pelanggan.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+	/* Nama  : Sinta Nurhalifah
+   	NPM      : 2117051015
+	Kelas    : B 2021 */
+	
+	//string//
+	string Nama;
+	string Alamat;
+	string Email;
+	string Nomor;
+	
+	//input//
+	cout<<"Nama Pelanggan  : ";
+	getline(cin,Nama);
+	cout<<"Alamat Rumah    : ";
+	getline(cin,Alamat);
+	cout<<"Email Pelanggan : ";
+	getline(cin,Email);
+	cout<<"Nomor Telepon   : ";
+	getline(cin,Nomor);
+	
+	
+	
+	//output//
+	cout<<endl;
+	cout<<"Nama Pelanggan "<<Nama <<endl;
+	cout<<"Alamat Rumah "<<Alamat <<endl;
+	cout<<"Email Pelanggan "<<Email <<endl;
+	cout<<"Nomor Telepon "<<Nomor <<endl;
+		return 0;
+	
+}
