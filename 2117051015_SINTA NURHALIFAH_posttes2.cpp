34be832240b7f6ba0204b17737335a8bc689e935
SINTA NURHALIFAH 2117051015 <92354983+sinta23nurhalif@users.noreply.github.com>
2021-12-19 19:55:44 +0700

    Add files via upload

---
+++ 2117051015_SINTA NURHALIFAH_posttes2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/*NAMA : SINTA NURHALIFAH
+NPM    : 2117051015
+KELAS  : B*/
+
+int main(){
+	float jumlah_bitcoin, harga_beli, harga_saat_ini,rumus_total, rumus_persen;
+	
+	cout<<"jumlah bitcoin : ";cin>>jumlah_bitcoin;
+	cout<<"harga pembelian : ";cin>>harga_beli;//harga awal
+	cout<<"harga saat ini : ";cin>>harga_saat_ini;//harga akhir
+	
+	cout<<"kenaikan/penurunan : "<<(( harga_saat_ini - harga_beli)/harga_beli)*100<<"%\n";
+	cout<<"keuntungan/kerugian :"<<(harga_saat_ini - harga_beli)/1000000<< " juta\n";	
+}
