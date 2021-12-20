13cb444b1ae511ab63c5007a062cd73a049d78b4
SINTA NURHALIFAH 2117051015 <92354983+sinta23nurhalif@users.noreply.github.com>
2021-12-20 09:58:00 +0700

    Add files via upload

---
+++ 2117051015_SintaNurhalifah_pretest2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	/*Nama : Sinta Nurhalifah
+	Npm : 2117051015
+	Kelas : B*/
+	
+	//KALKULATOR KONVERENSI MATA UANG RUPIAH KE ASET DIGITAL BITCOIN//
+	
+	//string//
+	string TanggalTransaksi ;
+	string BulanTransaksi ;
+	string TahunTransaksi ;
+	float JumlahUang ;
+	float HargaBitCoin ;
+	
+	
+	//input//
+	cin>> TanggalTransaksi ;
+	cin>> BulanTransaksi ;
+	cin>> TahunTransaksi ;
+	cin>> JumlahUang ;
+	cin>> HargaBitCoin ;
+	
+	
+	//output//
+	cout<< endl ;
+	cout<< "Tanggal Transaksi : "<< TanggalTransaksi << " " << BulanTransaksi << " " << TahunTransaksi << endl ;
+	cout<< "Jumlah Bitcoin    : "<< JumlahUang/HargaBitCoin << " " << "BitCoin" << endl ;
+
+	
+return 0;	
+}
+	
+	
+	
+	
+
+
