9bbbe03b9b1ca6811d963236fe5273c61ec3a24f
SINTA NURHALIFAH 2117051015 <92354983+sinta23nurhalif@users.noreply.github.com>
2022-01-22 11:30:08 +0700

    Update Bitcoin.cpp

--- Bitcoin.cpp
+++ Bitcoin.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-	
 	float jumlah_bitcoin, harga_beli, harga_saat_ini,rumus_total, rumus_persen;
-	
+
 	cout<<"jumlah bitcoin : ";cin>>jumlah_bitcoin;
 	cout<<"harga pembelian : ";cin>>harga_beli;//harga awal
 	cout<<"harga saat ini : ";cin>>harga_saat_ini;//harga akhir
